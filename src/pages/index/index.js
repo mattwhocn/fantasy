22def40e8be84d8e96b23b024c5671e238c322a1
zhangmeng <mattwho@163.com>
2018-05-04 15:30:12 +0800

    make some change

--- src/pages/index/index.js
+++ src/pages/index/index.js
@@ -2,8 +2,33 @@ require('./index.scss')
 var $ = require('jquery')
 var catalogList = require('utils/data/index.js')
 
+var homePage = {
+  init(){
+    this.onLoad()
+  },
+  onLoad() {
+    this._initCatalogList()
+  },
+  bindEvent() {
+
+  },
+
+  _initCatalogList() {
+    console.log(catalogList)
+    let catalogItem = '<li class="catalog-item">' + 
+                        '<div class="module-l"></div>' + 
+                        '<div class="module-r"></div>' + 
+                        '<div class="module-t"></div>' + 
+                      '</li>'
+    catalogList.forEach(() => {
+      
+    })                 
+  }
+
+}
+
 $(function() {
-  
+  homePage.init()
 })
 
 
---
+++ src/utils/data/index.js
@@ -0,0 +1,11 @@
+var catalogList = [
+  {
+    name: 'diyipian',
+    id: 1
+  },
+  {
+    name: 'diyipian2',
+    id: 2
+  }
+]
+module.exports = catalogList
